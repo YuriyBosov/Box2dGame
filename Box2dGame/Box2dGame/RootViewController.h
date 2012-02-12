a2c6311b7c382e5130180c50728ed838b87d3b74
Yuriy Bosov <bosov.yu@gmail.com>
2012-02-12 07:14:05 +0200

    add project files

---
+++ Box2dGame/Box2dGame/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Box2dGame
+//
+//  Created by Yuriy Bosov on 2/11/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
